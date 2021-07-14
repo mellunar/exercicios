bb544df80bf0556f98bbddaf50cf62f5f976a90d
mellunar <87154023+mellunar@users.noreply.github.com>
2021-07-14 13:27:31 -0300

    Update sinal.js

--- sinal.js
+++ sinal.js
@@ -5,61 +5,54 @@ window.addEventListener("load", () => {
         let damarelo = document.getElementById("amarelo");
         let dvermelho = document.getElementById("vermelho");
 
-        function configurar(elemento, texto, bckcolor, cor) {
-        elemento != "" || texto != "" ? elemento.innerHTML = texto : ""
-        elemento != "" && bckcolor != "" ? elemento.style.backgroundColor = bckcolor : ""
-        elemento != "" && cor != "" ? elemento.style.color = cor : ""
-        }
+        function configurar(elemento, texto, bckcolor, cor){
+            elemento != "" || texto != "" ? elemento.innerHTML = texto : "";
+            elemento != "" && bckcolor != "" ? elemento.style.backgroundColor = bckcolor : "";
+            elemento != "" && cor != "" ? elemento.style.color = cor : "";
+            }
         
         function verde(){
-        let tempo = 15;
-        setInterval(function(){
-        if (tempo > -1){
-            configurar(dcron,"","#496914", "#fff")
-            dcron.innerHTML = tempo;            
-            configurar(dverde, "Boa Viagem", "#496914", "#fff")
-            configurar(damarelo, "", "#333", "")
-            configurar(dvermelho, "", "#333", "")
-        }
-        tempo--;
-        },1000);
-        }
-    verde();
-    setTimeout(amarelo,16000);
-    setTimeout(vermelho,20000);
-    function amarelo(){
-        function cronometro(){
-            let tempo = 3;
-            setInterval(function(){
-                if (tempo > -1){
-                    configurar(dcron,"","#d1b60d", "#000")
-                    dcron.innerHTML = tempo;            
-                    configurar(dverde, "", "#333", "")
-                    configurar(damarelo, "Atenção", "#d1b60d", "#000")
-                    configurar(dvermelho, "", "#333", "")
-                }
-                tempo--;
-            },1000);
-        }
-        cronometro();
-    }
-    function vermelho(){
-        function cronometro(){
             let tempo = 15;
             setInterval(function(){
-                if (tempo > -1){
-                    configurar(dcron,"","#9e0b0f", "#fff")
-                    dcron.innerHTML = tempo;            
-                    configurar(dverde, "", "#333", "")
-                    configurar(damarelo, "", "#333", "")
-                    configurar(dvermelho, "Pare", "#9e0b0f", "#fff")
-                }
-                tempo--;
-            },1000);
-            
+            if (tempo > -1){
+                configurar(dcron,"","#496914", "#fff")
+                dcron.innerHTML = tempo;            
+                configurar(dverde, "Boa Viagem", "#496914", "#fff")
+                configurar(damarelo, "", "#333", "")
+                configurar(dvermelho, "", "#333", "")
+            }
+            tempo--;
+            },1000)
         }
-        cronometro();
-    }
+        function amarelo(){   
+            let tempo = 3;         
+            setInterval(function(){
+            if (tempo > -1){
+                configurar(dcron,"","#d1b60d", "#000")
+                dcron.innerHTML = tempo;            
+                configurar(dverde, "", "#333", "")
+                configurar(damarelo, "Atenção", "#d1b60d", "#000")
+                configurar(dvermelho, "", "#333", "")
+            }
+            tempo--;
+            },1000)
+        }
+        function vermelho(){ 
+            let tempo = 15;         
+            setInterval(function(){
+            if (tempo > -1){
+                configurar(dcron,"","#9e0b0f", "#fff")
+                dcron.innerHTML = tempo;            
+                configurar(dverde, "", "#333", "")
+                configurar(damarelo, "", "#333", "")
+                configurar(dvermelho, "Pare", "#9e0b0f", "#fff")
+            }
+            tempo--;
+            },1000)
+        }        
+        verde();
+        setTimeout(amarelo, 16000);
+        setTimeout(vermelho, 20000);
     }
     loop();
     setInterval(loop, 36000)
